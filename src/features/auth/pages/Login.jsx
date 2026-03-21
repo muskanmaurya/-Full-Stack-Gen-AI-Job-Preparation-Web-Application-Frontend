90cb650bfc31b312d94a3e3f9af5208e4e2e4271
muskan maurya <muskanmaurya0987@gmail.com>
2026-03-21 12:41:44 +0530

    first commit basic UI

---
+++ src/features/auth/pages/Login.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import '../auth.form.scss'
+import "../../../style/button.scss"
+import { Link } from 'react-router-dom'
+import {useAuth} from "../hooks/useAuth"
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+const Login = () => {
+
+    const {loading,handleLogin} = useAuth();
+    const navigate=useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+
+    const handlesubmit = async (e) => {
+        e.preventDefault()
+        await handleLogin({email,password});
+        navigate("/");
+    }
+
+    if(loading){
+        return (<main><h1>Loading...</h1></main>)
+    }
+  return (
+    <main className='bg-amber-300'>
+        <div className="form-container"> 
+            <h1>Login</h1>
+            <form onSubmit={handlesubmit}>
+                <div className="input-group">
+                    <label htmlFor='email'>Email</label>
+                    <input 
+                    onChange={(e)=>{setEmail(e.target.value)}}
+                    type="email" id='email' placeholder='Enter your Email'/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor='password'>Password</label>
+                    <input 
+                    onChange={(e)=>{setPassword(e.target.value)}}
+                    type="password" id='password' placeholder='Enter your Password'/>
+                </div>
+                <button className='button primary-button'>Login</button>
+            </form>
+            <p>Don't have an account?<Link to="/register">Register</Link></p>
+
+        </div>
+    </main>
+  )
+}
+
+export default Login
---
+++ src/features/auth/pages/Register.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import {  Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import {useAuth} from "../hooks/useAuth"
+
+const Register = () => {
+    const navigate=useNavigate();
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+
+    const {loading,handleRegister}=useAuth();
+
+  const handlesubmit = async(e) => { 
+        e.preventDefault()
+        await handleRegister({username,email,password});
+        navigate("/");
+    }
+
+    if(loading){
+        return (<main><h1>Loading...</h1></main>)
+    }
+  return (
+    <main>
+        <div className="form-container"> 
+            <h1>Register</h1>
+            <form onSubmit={handlesubmit}>
+                <div className="input-group">
+                    <label htmlFor='username'>Username</label>
+                    <input 
+                    onChange={(e)=>{setUsername(e.target.value)}}
+                    type="text" id='username' placeholder='Enter your Username'/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor='email'>Email</label>
+                    <input 
+                    onChange={(e)=>{setEmail(e.target.value)}}
+                    type="email" id='email' placeholder='Enter your Email'/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor='password'>Password</label>
+                    <input 
+                    onChange={(e)=>{setPassword(e.target.value)}}
+                    type="password" id='password' placeholder='Enter your Password'/>
+                </div>
+                <button className='button primary-button'>Register</button>
+            </form>
+            <p>Already have an account?<Link to="/login">Login</Link></p>
+        </div>
+    </main>
+  )
+}
+
+export default Register
